9846ec2d7f92167b11e43c3e4afa470ac3d850ba
TaigaShiokawa(塩川泰雅) <TaigaShiokawa()@Dirbato-1418x>
2024-04-25 12:31:03 +0900

    User情報更新

--- TutorialDevelop/src/main/java/com/techacademy/service/UserService.java
+++ TutorialDevelop/src/main/java/com/techacademy/service/UserService.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.techacademy.entity.User;
 import com.techacademy.repository.UserRepository;
+
+
 
 @Service
 public class UserService {
@@ -17,5 +20,20 @@ public class UserService {
     public List<User> getUserList() {
         // リポジトリのfindAllメソッドを呼び出す
         return userRepository.findAll();
+    }
+    
+
+    /** Userを1件検索して返す */
+    public User getUser(Integer id) {
+        return userRepository.findById(id).get();
+    }
+
+
+
+    /** Userの登録を行う */
+    @Transactional 
+    public User saveUser(User user) 
+    {
+        return userRepository.save(user);
     }
 }
